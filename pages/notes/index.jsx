59266fd7cd34b2265ca93c8093fd9de669646e0a
slavo3dev <slavo@slavo3.com>
2021-02-20 16:47:22 -0500

    adding routes

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Link from "next/link";
 
 const Notes = () => {
+  const notes = new Array(15)
+    .fill(1)
+    .map((e, i) => ({ id: i, title: `Note: ${i}` }));
+
   return (
     <div>
-      <h1>Notes PAGE </h1>
-      <Link href="./notes/[id]" as={`./notes/1`}>
-        <a>Note PAGE</a>
-      </Link>
+      <h1>Notes: </h1>
+      {notes.map((note) => (
+        <div key={note.id}>
+          <Link href="./notes/[id]" as={`./notes/${note.id}`}>
+            <a>
+              <strong>{note.title}</strong>
+            </a>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
