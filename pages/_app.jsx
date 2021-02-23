7c63bb7b6002fcd9b73a565f34f5962cb4c57348
slavo3dev <slavo@slavo3.com>
2021-02-22 20:35:58 -0500

    importing nav

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "theme-ui";
 import theme from "../theme";
+import Nav from "../src/components/nav";
 
 export default function App({ Component, pageProps }) {
   return (
